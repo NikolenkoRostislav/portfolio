19d9f6c425aa239f1a811333b00eeddaa06c1a5f
RSlavN <rostislavnikolenkowork@gmail.com>
2025-04-25 23:34:25 +0200

    Feat: added context for dark theme and language settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import {Routes, Route} from 'react-router';
+import { useState, useEffect, createContext } from 'react'
+import { Routes, Route } from 'react-router';
 
 import Layout from './Layout.jsx';
 import Home from './Pages/Home.jsx';
 import About from './Pages/About.jsx';
 
+export const Context = createContext(null);
+
 export default function App() {
-  return (
-    <>
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
-                <Route path="about" element={<About />} />
-            </Route>
-        </Routes>
-    </>
-  )
+    const [darkTheme, setDarkTheme] = useState(true);
+    const [language, setLanguage] = useState("Deutsch");
+    function changeTheme() {setDarkTheme(!darkTheme)}
+    function changeLang() {setLanguage(language === "English" ? "Deutsch" : "English")}
+
+    useEffect(() => {
+    //Save this in cookies or something, haven't quite figured it out yet 
+    }, [language, darkTheme]);
+
+    return (
+        <>
+            <Context.Provider value={{darkTheme, language, changeTheme, changeLang}} >
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="about" element={<About />} />
+                    </Route>
+                </Routes>
+            </Context.Provider>
+        </>
+    )
 }
