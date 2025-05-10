2167d2115e109f9c4d689d2bb0d7e4c3ddfebddf
RSlavN <rostislavnikolenkowork@gmail.com>
2025-05-10 15:22:07 +0200

    Style: added basic styling for the nav

--- src/Sections/Navbar.jsx
+++ src/Sections/Navbar.jsx
@@ -1,42 +1,30 @@
 import { useContext } from 'react';
 import { Link } from "react-router";
-
 import { Context } from '../App.jsx';
 
 export default function Navbar() {
     const context = useContext(Context);
 
-    const links = [ //I should redo the languages implementation with i18next, this is for testing and not final
-        { nameDE: "Startseite", nameEN: "Home", link: "/"},
-        { nameDE: "Über mich", nameEN: "About", link: "/about"},
-        { nameDE: "Kontakt", nameEN: "Contact Me", link: "/contacts"},
+    const links = [
+        {name: "Home", link: "/"},
+        {name: "About", link: "/about"},
+        {name: "Contact Me", link: "/contacts"},
     ];
 
     return (
         <nav>
-            <>{context.language === "English" ? <>       
+            <div className="nav-left">    
                 <h1>Rostislav Nikolenko</h1>
-
                 <ul>
                     {links.map((link, index) => (
-                        <li key={index}><Link to={link.link}>{link.nameEN}</Link></li>
+                        <li key={index}><Link to={link.link}>{link.name}</Link></li>
                     ))}
                 </ul>
-
+            </div>
+            <div className="nav-right">
                 <button onClick={context.changeTheme}>{context.darkTheme ? "Dark" : "Light"}</button> 
-                <button onClick={context.changeLang}>{context.language}</button> 
-            </> : <>       
-                <h1>Rostislav Nikolenko</h1>
-
-                <ul>
-                    {links.map((link, index) => (
-                        <li key={index}><Link to={link.link}>{link.nameDE}</Link></li>
-                    ))}
-                </ul>
-
-                <button onClick={context.changeTheme}>{context.darkTheme ? "Dunkel" : "Hell"}</button> 
-                <button onClick={context.changeLang}>{context.language}</button> </>}
-            </>
+                <button onClick={context.changeLang}>{context.language}</button>  
+            </div>
         </nav>
     )
 }
